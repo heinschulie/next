c77ced5c382dd5af0318b46a0c8fa2908e4f1631
heinschulie <heinschulie@gmail.com>
2023-05-13 12:33:46 +0200

    mirror setup of brcs

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -47,15 +47,7 @@
 
 <svelte:head>
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-M644WT5');</script>
-	<!-- End Google Tag Manager -->
-
-	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-M644WT5"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=GTM-M644WT5"></script>
 	<script>
 		window.dataLayer = window.dataLayer || []
 
@@ -67,7 +59,7 @@
 		gtag('config', 'GTM-M644WT5', {
 			page_path: window.location.pathname
 		})
-	</script> -->
+	</script>
 
 	<title>{ title }</title>
 	<meta property="og:determiner" content="The">
