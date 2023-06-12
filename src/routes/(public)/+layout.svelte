a4910d4428f7da2aa4e531fb2d9f94d02f6f8552
heinschulie <heinschulie@gmail.com>
2023-06-12 10:25:23 +0200

    ga test old school

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -21,36 +21,29 @@
 	import '../styles.css';
 	import { browser } from '$app/environment';
 
-		const gtag = `
-		window.dataLayer = window.dataLayer || []
-
-		function gtag() {
-			dataLayer.push(arguments)
-		}
-
-		gtag('js', new Date())
-		gtag('config', 'GTM-M644WT5', {
-			page_path: window.location.pathname
-		})
-		`;
-		const googletagmanager = `https://www.googletagmanager.com/gtag/js?id=GTM-M644WT5`;
-
-
-		// --> spiffy sprinkles : GTM-MN78JMW 
-		// --> cs  				: GTM-M644WT5
-		let scriptEl: any;
-		onMount(
-			() => {
-				if (scriptEl) {
-					scriptEl.textContent = partytownSnippet()
-				}
+	// --> spiffy sprinkles : GTM-MN78JMW 
+	// --> cs  				: GTM-M644WT5
+	let scriptEl: any;
+	onMount(
+		() => {
+			if (scriptEl) {
+				scriptEl.textContent = partytownSnippet()
 			}
-		)
+		}
+	)
 		
 </script>
 
 <svelte:head>
 
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-MN78JMW');</script>
+	<!-- End Google Tag Manager -->
+		
 	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-M644WT5"></script>
 	<script>
 		window.dataLayer = window.dataLayer || []
@@ -65,8 +58,8 @@
 		})
 	</script> -->
 
+	<!-- exp partytown -->
 	<script>
-		// Config options
 		partytown = {
 		  forward: ['dataLayer.push'],
 		  resolveUrl: (url) => {
@@ -92,13 +85,9 @@
 		  }
 		}
 	</script>
-	  
-	<!-- Insert `partytownSnippet` here -->
 	<script bind:this={scriptEl}>
 	</script>
-	
-	  <!-- GTM script + config -->
-	  <script
+	<script
 		type="text/partytown"
 		src="https://www.googletagmanager.com/gtag/js?id=GTM-MN78JMW">
 	
@@ -116,6 +105,7 @@
 			page_path: window.location.pathname
 		})
 	</script>
+	<!-- end partytown -->
 
 
 	<title>{ title }</title>
