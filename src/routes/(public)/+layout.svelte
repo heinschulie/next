440e0bae9c5e88af41264083c0dbe9ecee63bb41
heinschulie <heinschulie@gmail.com>
2023-06-12 11:24:18 +0200

    removed old school

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -41,22 +41,22 @@
 <svelte:head>
 
 	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-MN78JMW');</script>
+	})(window,document,'script','dataLayer','GTM-MN78JMW');</script> -->
 	<!-- End Google Tag Manager -->
 		
 	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-L07EDRYQHH"></script>
+	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-L07EDRYQHH"></script>
 	<script>
 	window.dataLayer = window.dataLayer || [];
 	function gtag(){dataLayer.push(arguments);}
 	gtag('js', new Date());
 
 	gtag('config', 'G-L07EDRYQHH');
-	</script>
+	</script> -->
 
 	<!-- exp partytown -->
 	<!-- <script>
