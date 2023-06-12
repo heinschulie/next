3cdfcf1759857e1e1793feb842cdc6884dfc1e75
heinschulie <heinschulie@gmail.com>
2023-06-12 13:12:34 +0200

    docs way

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -152,8 +152,26 @@
 
 	<script bind:this={scriptEl}></script>
 
+	<script
+		type="text/partytown"
+		src="https://www.googletagmanager.com/gtag/js?id=GTM-MN78JMW"></script>
+	<script type="text/partytown">
+		window.dataLayer = window.dataLayer || []
+
+		function gtag() {
+		dataLayer.push(arguments)
+		}
+
+		gtag('js', new Date())
+		gtag('config', 'GTM-MN78JMW', {
+		page_path: window.location.pathname
+		})
+	</script>
+
+
+
 	{#if gtmDebug}
-		<script>
+		<!-- <script>
 			(function (w, d, s, l, i) {
 				w[l] = w[l] || [];
 				w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
@@ -164,7 +182,7 @@
 				j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
 				f.parentNode.insertBefore(j, f);
 			})(window, document, 'script', 'dataLayer', 'GTM-MN78JMW');
-		</script>
+		</script> -->
 
 		<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-MN78JMW"></script>
 		<script>
@@ -180,7 +198,7 @@
 			})
 		</script> -->
 	{:else}
-		<script type="text/partytown">
+		<!-- <script type="text/partytown">
 			(function (w, d, s, l, i) {
 				w[l] = w[l] || [];
 				w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
@@ -191,7 +209,7 @@
 				j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
 				f.parentNode.insertBefore(j, f);
 			})(window, document, 'script', 'dataLayer', 'GTM-MN78JMW');
-		</script>
+		</script> -->
 
 		<!-- <script type="text/partytown" async src="https://www.googletagmanager.com/gtag/js?id=GTM-MN78JMW"></script>
 		<script type="text/partytown">
