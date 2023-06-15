ae064cb284787a261ce468bb4dec456472b813e4
heinschulie <heinschulie@gmail.com>
2023-06-15 11:01:18 +0200

    test api

---
+++ src/routes/api/analytics/+server.ts
@@ -0,0 +1,25 @@
+import { json } from '@sveltejs/kit';
+
+export async function GET({ url }) {
+	const platform = url.searchParams.get('platform');
+
+	console.log('FLYING AROUND THE HOUSE: ', platform);
+
+	let newUrl = '';
+	switch (platform) {
+		case 'facebook': {
+			newUrl = 'https://connect.facebook.net/en_US/fbevents.js';
+			break;
+		}
+		default: {
+			return json({}, { status: 401 });
+		}
+	}
+
+	const res = await fetch(newUrl);
+	console.log('I HAVE YOUR GOOD CLOTHES IN THE CAR: ', res);
+	const posts = await res.json();
+	console.log('ALL MY FINGERNAILS ARE PAINTED: ', posts);
+
+	return json(posts, { status: 200 });
+}
